369fb599c1c67433dd8bdc9941ee3fb41c0cae37
Wuming <zhuwumingemail@gmail.com>
2017-10-04 15:20:32 +0800

    更新了int数据逆转的程序，测试速度加快。

--- LeetCode/src/Reverse.java
+++ LeetCode/src/Reverse.java
@@ -27,7 +27,50 @@ public class Reverse {
 
     }
 
+    public static int reverse1(int x) {
+        boolean isPos = true;
+        int num = Math.abs(x);
+        long result = 0;
+        long[] number = new long[10];
+        if (x < 0) {
+            isPos = false;
+        }
+        int i;
+        for (i = 0; i < number.length && num != 0; i++) {
+            number[i] = num % 10;
+            num = num / 10;
+        }
+        for (int k = 0; k < i; k++) {
+            for (int j = i - k - 2; j >= 0; j--) {
+                number[k] = number[k] * 10;
+            }
+        }
+        for (int j = 0; j < i; j++) {
+            result += number[j];
+
+        }
+        if (!isPos) {
+            result = (-1) * result;
+        }
+        if (result > Integer.MAX_VALUE || result < Integer.MIN_VALUE) {
+            return 0;
+        }
+        return (int) result;
+    }
+
+    public int reverseMostLeastTime(int x) {
+        int res = 0;
+        while (x != 0) {
+            int newRes = res * 10 + x % 10;
+            if ((newRes - x % 10) / 10 != res)
+                return 0;
+            res = newRes;
+            x /= 10;
+        }
+        return res;
+    }
+
     public static void main(String[] args) {
-        System.out.println(reverse(47483647));
+        System.out.println(reverse1(2136478999));
     }
 }
